5bd1f37368ec364a6dc077c29f1a7f6b21825c35
Dhruv Moradiya <dmoradiya443@gmail.com>
2025-02-27 21:33:42 +0530

    Add Notification model

---
+++ models/notification.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const NotificationSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receivers: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    ],
+
+    notificationType: {
+      type: String,
+      enum: ["MESSAGE", "MENTION", "ATTACHMENT", "APP_NOTIFICATION"],
+      required: true,
+    },
+
+    content: { type: String, required: true },
+
+    message: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
+
+    attachments: [
+      {
+        url: { type: String },
+        fileType: {
+          type: String,
+          enum: ["image", "video", "document", "audio"],
+        },
+      },
+    ],
+
+    isRead: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model("Notification", NotificationSchema);
+
+export default Notification;
